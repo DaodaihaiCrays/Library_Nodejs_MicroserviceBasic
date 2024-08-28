915d7ab8e91b4241d3f682951962601a15afaa97
Haidaidao <hai0944345963@gmail.com>
2024-08-28 10:49:02 +0700

    parse body id object in order

--- orders/service/ordersService.js
+++ orders/service/ordersService.js
@@ -14,13 +14,33 @@ const CreateOrdersService = async (orders) => {
 
 const GetAllOrdersService = async(data) => {
     try {
-        const result = await Orders.find()
-        return result 
+        const result = await Orders.find();
+        console.log(result);
+
+        for (let i = 0; i < result.length; i++) {
+            const resCustomer = await axios.get("http://localhost:3001/customers/" + result[i]['CustomerId'].toString());
+            const resBook = await axios.get("http://localhost:3000/book/" + result[i]['BookId'].toString());
+           
+           
+            const customerData = resCustomer.data['data'];
+            const bookData = resBook.data['data'];
+            
+            if(!customerData || !bookData)
+                return null
+
+            result[i] = {
+                ...result[i]._doc, // để xử lý kq trả về không chứa các field dư của Mongoose
+                CustomerId: customerData,
+                BookId: bookData,
+            };
+        }
+
+        return result;
     } catch (error) {
-        console.error('Error fetching all Orders:', error) 
-        return null 
+        console.error('Error fetching all Orders:', error);
+        return null;
     }
-}
+};
 
 const GetOrdersByIdService = async(id) => {
     try {
